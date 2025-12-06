9f31ba1b86bcb7813f67b4e4de02475f22b9d66e
huyanyou <1026855639@qq.com>
2025-12-06 21:03:35 +0800

    授权

--- biology-admin/src/main/java/com/biology/admin/controller/manage/KongTiaoDataController.java
+++ biology-admin/src/main/java/com/biology/admin/controller/manage/KongTiaoDataController.java
@@ -4,14 +4,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.biology.common.core.base.BaseController;
 import com.biology.common.core.page.PageDTO;
 import com.biology.common.utils.poi.CustomExcelUtil;
@@ -23,11 +15,24 @@ import com.biology.domain.manage.kongTiaoData.dto.KongTiaoDataDTO;
 import com.biology.domain.manage.kongTiaoData.query.KongTiaoDataQuery;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Tag(name = "知识库API", description = "知识库的增删查改")
 @RestController
 @RequestMapping("/manage/kongTiaoData")
+@Validated
 @RequiredArgsConstructor
 public class KongTiaoDataController extends BaseController {
     private final KongTiaoDataApplicationService kongTiaoDataApplicationService;
