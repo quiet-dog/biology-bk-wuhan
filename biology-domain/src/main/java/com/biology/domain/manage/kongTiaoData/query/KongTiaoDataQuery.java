b0e05fa6f8c4815ef8ae70aa196082e0e8b8e155
huyanyou <1026855639@qq.com>
2025-12-07 15:51:49 +0800

    授权

--- biology-domain/src/main/java/com/biology/domain/manage/kongTiaoData/query/KongTiaoDataQuery.java
+++ biology-domain/src/main/java/com/biology/domain/manage/kongTiaoData/query/KongTiaoDataQuery.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.biology.common.core.page.AbstractPageQuery;
 import com.biology.domain.manage.kongTiaoData.db.KongTiaoDataEntity;
 
+import cn.hutool.core.util.StrUtil;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -16,6 +17,7 @@ public class KongTiaoDataQuery extends AbstractPageQuery<KongTiaoDataEntity> {
     @Override
     public QueryWrapper<KongTiaoDataEntity> addQueryCondition() {
         QueryWrapper<KongTiaoDataEntity> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq(!StrUtil.isEmpty(deviceSn), "device_sn", deviceSn);
 
         setTimeRangeColumn("create_time");
         setOrderColumn("create_time");
